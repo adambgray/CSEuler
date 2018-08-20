1856b186bfb17ef952ef10e13e128aa40fc934f4
Adam Gray <adambgray@gmail.com>
2018-08-20 18:52:44 -0400

    Completed solution for Project Euler problem 19

---
+++ Euler19/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Euler19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(1901, 1, 1);
+            DateTime endDate = new DateTime(2000, 12, 31);
+            TimeSpan oneDay = new TimeSpan(1, 0, 0, 0);
+            TimeSpan oneWeek = new TimeSpan(7, 0, 0, 0);
+            int counter = 0;
+
+            while(date.DayOfWeek != DayOfWeek.Sunday)
+            {
+                date += oneDay;
+            }
+
+            while(date <= endDate)
+            {
+                if(date.Day == 1)
+                {
+                    counter++;
+                }
+                date += oneWeek;
+            }
+
+            Console.WriteLine(counter);
+            Console.ReadLine();
+        }
+    }
+}
